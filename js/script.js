ad841c24f8dd6e8cfc8e5df81ad739b4d3517339
Muhammad Abid Dewantoro <151120880+DeadEye27@users.noreply.github.com>
2026-03-20 05:19:38 +0700

    Connect this Frontend to Local Backend
    Sudah bisa koneksi antara frontend ini dengan backend local yang sudah disetup, dan juga sudah bisa mengambil semua notes yang ada di database menggunakan fetch di file script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ function openForm() {
 function closeForm() {
     document.getElementById('myForm').style.display = 'none';
 }
+
+// GET ALL Notes
+fetch("http://localhost:3000/notes")
+    .then(res => {
+        return res.json();
+        console.log(res);
+        
+    })
+    .then(data => {
+        console.log(data);
+        const Data = data.data;
+        console.log(Data);
+        Data.forEach((notes, index) => {
+            const markupCard = `<div class="card" id="note${index+1}"></div>`;
+            document.getElementById('card-container').insertAdjacentHTML('beforeend', markupCard);
+
+            const markupTitle = `<div class="card-title">${notes.title}</div>`;
+            document.getElementById(`note${index+1}`).insertAdjacentHTML('beforeend', markupTitle);
+
+            const markupContent = `<div class="card-note-section">${notes.userNote}</div>`;
+            document.getElementById(`note${index+1}`).insertAdjacentHTML('beforeend', markupContent);
+
+            const markupButton = `<div class="card-button">pin, edit, delete</div>`;
+            document.getElementById(`note${index+1}`).insertAdjacentHTML('beforeend', markupButton);
+        });
+    })
